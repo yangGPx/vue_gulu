dfdc6ca5f14c56552477d5d2fa45d38f31ac41be
yangGPx <1299563700@qq.com>
2019-12-22 17:40:29 +0800

    整理之前的组件  将放在parcel运行的目录  放在vue-cli3上

--- src/main.js
+++ src/main.js
@@ -1,5 +1,52 @@
 import Vue from 'vue';
 import App from './App.vue';
+import Button from './components/GuluButton.vue';
+import Icon from './components/GuluIcon.vue';
+import ButtonFroup from './components/buttonGroup.vue';
+import GridRow from './components/grid/GridRow.vue';
+import GridCol from './components/grid/GridCol.vue';
+import GridLayout from './components/layoutC/Layout.vue';
+import GridHeader from './components/layoutC/Header.vue';
+import GridContent from './components/layoutC/Content.vue';
+import GridSlider from './components/layoutC/Slider.vue';
+import GridFooter from './components/layoutC/Footer.vue';
+import Tabs from './components/tabs/Tabs.vue';
+import TabsHead from './components/tabs/TabsHead.vue';
+import TabsBody from './components/tabs/TabsBody.vue';
+import TabsHeadItem from './components/tabs/TabsHeadItem.vue';
+import TabsBodyPane from './components/tabs/TabsBodyPane.vue';
+import plugin from './components/ToastPlugin';
+import PopOver from './components/PopOver.vue';
+import Collapse from './components/collapse/Collapse.vue';
+import CollapseItem from './components/collapse/CollapseItem.vue';
+import Cascader from './components/Cascader.vue';
+import CascaderItem from './components/CascaderItem.vue';
+import Slider from './components/GuluSlider.vue';
+import SliderItem from './components/GuluSliderItem.vue';
+
+Vue.use(plugin);
+Vue.component('g-button', Button)
+Vue.component('g-icon', Icon)
+Vue.component('g-button-group', ButtonFroup)
+Vue.component('g-row', GridRow)
+Vue.component('g-col', GridCol)
+Vue.component('g-layout', GridLayout)
+Vue.component('g-layout-header', GridHeader)
+Vue.component('g-layout-content', GridContent)
+Vue.component('g-layout-slider', GridSlider)
+Vue.component('g-layout-footer', GridFooter)
+Vue.component('g-tabs', Tabs)
+Vue.component('g-tabs-head', TabsHead)
+Vue.component('g-tabs-body', TabsBody)
+Vue.component('g-tabs-item', TabsHeadItem)
+Vue.component('g-tabs-pane', TabsBodyPane)
+Vue.component('g-popover', PopOver)
+Vue.component('g-collapse', Collapse)
+Vue.component('g-collapse-item', CollapseItem)
+Vue.component('g-cascader', Cascader)
+Vue.component('g-cascader-item', CascaderItem)
+Vue.component('g-slider', Slider)
+Vue.component('g-slider-item', SliderItem)
 
 new Vue({
   render: h => h(App),
